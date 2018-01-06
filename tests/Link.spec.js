c11dbbadb6a524a5fe691551a2c49f06771a61a6
Mo <m@9dev.de>
2018-01-06 21:16:11 +0100

    Added link tests, fixed errors, made target argument optional

---
+++ tests/Link.spec.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/*
+ global module,
+ require,
+ exports,
+ describe,
+ it
+ */
+
+const expect = require( 'chai' ).expect;
+
+const Element = require( '../lib/document/element/Element' ),
+      Link    = require( '../lib/document/element/Link' );
+
+describe( 'Link elements', () => {
+  it( 'Should create links for other document nodes by reference', () => {
+    const root   = new Element,
+          target = new Element,
+          link   = new Link( target );
+
+    root.appendChild( target );
+    root.appendChild( link );
+
+    expect( link.getTarget() ).to.equal( target );
+  } );
+
+  it( 'Should create links for other document nodes by UID', () => {
+    const root   = new Element,
+          target = new Element,
+          link   = new Link();
+
+    root.appendChild( target );
+    root.appendChild( link );
+
+    link.setTarget( target.uid );
+
+    expect( link.getTarget() ).to.equal( target );
+  } );
+
+  it( 'Should bail on wrong UIDs', () => {
+    const root   = new Element,
+          target = new Element,
+          link   = new Link();
+
+    root.appendChild( target );
+    root.appendChild( link );
+
+    expect( () => link.setTarget( 'foo' ) ).to.throw( TypeError );
+  } );
+} );
