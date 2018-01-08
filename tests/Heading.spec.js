78b004b45af4a45a20699e5c7510a6fb9e494d1a
Mo <m@9dev.de>
2018-01-08 16:27:49 +0100

    Added heading elements

---
+++ tests/Heading.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+ global module,
+ require,
+ exports,
+ describe,
+ it
+ */
+
+const expect = require( 'chai' ).expect;
+
+const Heading = require( '../lib/document/element/Heading' ),
+      Text    = require( '../lib/document/element/Text' );
+
+describe( 'Heading elements', () => {
+  it( 'Should turn strings into text nodes', () => {
+    const element = new Heading( 'foo\nbar\n\nbaz\n\n\nquz' );
+
+    expect( element.textContent ).to.equal( 'foo\nbar\n\nbaz\n\n\nquz' );
+    expect( element.firstChild ).to.be.an.instanceof( Text );
+  } );
+
+  it( 'Should accept text nodes', () => {
+    const text    = new Text( 'foo bar baz' ),
+          heading = new Heading( text );
+
+    expect( heading.firstChild ).to.equal( text );
+  } );
+} );
