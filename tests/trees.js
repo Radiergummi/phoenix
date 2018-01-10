90e1b680592852351e6664497b6f60e75bd68e8c
Mo <m@9dev.de>
2018-01-10 09:03:35 +0100

    changed table child node check

---
+++ tests/trees.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+ global module,
+ require,
+ exports,
+ describe,
+ it
+ */
+
+const { expect } = require( 'chai' );
+
+describe('Document trees', () => {
+  it('Should create complex tree structures', () => {
+
+  });
+});
